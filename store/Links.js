2f44b774c0f610c3690d1e9652600dd99bdcd94d
Jasmine Zanders <jasminezanders211@gmail.com>
2022-12-12 12:21:19 -0600

    week6/7

---
+++ store/Links.js
@@ -0,0 +1,8 @@
+export default [
+  { title: "Home", text: "Home" },
+  { title: "Rate", text: "rate" },
+  { title: "Bio", text: "Bio" },
+  { title: "Map", text: "Map" },
+  { title: "Contact", text: "Contact" },
+  { title: "Links", text: "Links" }
+];
---
+++ store/Map.js
@@ -0,0 +1,4 @@
+export default {
+  header: "My Map Page",
+  view: "Map"
+};
