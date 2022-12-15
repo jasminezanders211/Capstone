ff77c356b89f717497a8cbf8910a461ba83c6b2f
Jasmine Zanders <jasminezanders211@gmail.com>
2022-12-15 10:07:25 -0600

    week7 homework

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,7 +1,6 @@
 import html from "html-literal";
 
-export default () => html`
-
+export default state => html`
   <div class="grid-container">
     <div class="grid-item grid-item-1">zoo animals</div>
     <div class="grid-item grid-item-2">cee animals</div>
@@ -9,7 +8,7 @@ export default () => html`
   </div>
   <p>still working on this.</p>
   <main>
-    <div smoothie="main-content">
+    <div>
       <h2 id="para1">Place Holder</h2>
       <ol>
         <li>place holder</li>
@@ -26,4 +25,9 @@ export default () => html`
       </ol>
     </div>
   </main>
+  <h4>
+    The weather in ${state.weather.city} is ${state.weather.description}.
+    Temperature is ${state.weather.temp}F, and it feels like
+    ${state.weather.feelsLike}F.
+  </h4>
 `;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import axios from "axios";
 
 const router = new Navigo("/");
 
@@ -16,6 +17,55 @@ function render(state = store.Home) {
 `;
   router.updatePageLinks();
 }
+router.hooks({
+  before: (done, params) => {
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home"; // Add a switch case statement to handle multiple routes
+    switch (view) {
+      case "Home":
+        axios
+          .get(
+            // Replace the key provided here with your own key from openweathermap
+            `https://api.openweathermap.org/data/2.5/weather?q=st%20louis&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}`
+          )
+          .then(response => {
+            const kelvinToFahrenheit = kelvinTemp =>
+              Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+            store.Home.weather = {};
+            store.Home.weather.city = response.data.name;
+            store.Home.weather.temp = kelvinToFahrenheit(
+              response.data.main.temp
+            );
+            store.Home.weather.feelsLike = kelvinToFahrenheit(
+              response.data.main.feels_like
+            );
+            store.Home.weather.description = response.data.weather[0].main;
+            done();
+          })
+          .catch(err => console.log(err));
+        break;
+      // New Case for Pizza View
+      case "Pizza":
+        // New Axios get request utilizing already made environment variable
+        axios
+          .get(`${process.env.PIZZA_PLACE_API_URL}/pizzas`)
+          .then(response => {
+            console.log(response.data); // Storing retrieved data in state
+            store.Pizza.pizzas = response.data;
+            done();
+          })
+          .catch(error => {
+            console.log("It puked", error);
+            done();
+          });
+        break;
+      default:
+        done();
+    }
+  }
+});
 router
   .on({
     "/": () => render(),
