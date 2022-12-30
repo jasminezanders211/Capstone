3421b4cff30ed63aaf6df77f23ad5f082ff4f99b
Jasmine Zanders <jasminezanders211@gmail.com>
2022-12-29 21:01:56 -0600

    MORE capstone work

--- components/views/Rate.js
+++ components/views/Rate.js
@@ -3,7 +3,7 @@ import html from "html-literal";
 export default () => html`
   <h1>Rate Your College</h1>
   <p>My first paragraph.</p>
-  <button id="myBtn">Rate</button>
+  <!-- <button id="myBtn">Rate</button> -->
   <!-- <div class="grid-container">
     <div class="grid-item grid-item-1">this can be better</div>
     <div class="grid-item grid-item-2">this can be better</div>
@@ -70,9 +70,25 @@ export default () => html`
       <option value="4">4</option>
       <option value="5">5</option>
     </select>
-    <input type="button" value="environmentRate" />
+    <label for="education">Education:</label>
+    <select id="education" name="education">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+    <label for="activities">Activities:</label>
+    <select id="activities" name="activities">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+    <input type="button" value="Rate" id="flovalleybtn" />
   </div>
-  <h2>Florissant Valley Community college</h2>
+  <h2>Washington University</h2>
   <div>
     <label for="environment">Environment:</label>
     <select id="environment" name="environment">
@@ -82,6 +98,106 @@ export default () => html`
       <option value="4">4</option>
       <option value="5">5</option>
     </select>
-    <input type="button" value="environmentRate" />
+    <label for="education">Education:</label>
+    <select id="education" name="education">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+    <label for="activities">Activities:</label>
+    <select id="activities" name="activities">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+    <input type="button" value="Rate" id="washUbtn" />
+  </div>
+  <h2>University of Missouri-St. Louis</h2>
+  <div>
+    <label for="environment">Environment:</label>
+    <select id="environment" name="environment">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+    <label for="education">Education:</label>
+    <select id="education" name="education">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+    <label for="activities">Activities:</label>
+    <select id="activities" name="activities">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+    <input type="button" value="Rate" id="umslbtn" />
+  </div>
+  <h2>Webster University</h2>
+  <div>
+    <label for="environment">Environment:</label>
+    <select id="environment" name="environment">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+    <label for="education">Education:</label>
+    <select id="education" name="education">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+    <label for="activities">Activities:</label>
+    <select id="activities" name="activities">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+    <input type="button" value="Rate" id="websterbtn" />
+  </div>
+  <h2>Harris-Stowe State University</h2>
+  <div>
+    <label for="environment">Environment:</label>
+    <select id="environment" name="environment">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+    <label for="education">Education:</label>
+    <select id="education" name="education">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+    <label for="activities">Activities:</label>
+    <select id="activities" name="activities">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+    <input type="button" value="Rate" id="harrisbtn" />
   </div>
 `;
